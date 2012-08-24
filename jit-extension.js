0483863b72049cc5a64855b6127492ace7d7f046
baiaboo <baiaboo@mit.edu>
2012-08-24 02:36:33 -0400

    adding files

---
+++ jit-extension.js
@@ -0,0 +1,62 @@
+/*==================================================
+ *  Simile Exhibit Jit Extension
+ *==================================================
+ */
+
+(function() {
+    var isCompiled = ("Exhibit_JitExtension_isCompiled" in window) && 
+                    window.Exhibit_JitExtension_isCompiled;
+                    
+    Exhibit.JitExtension = {
+        params: {
+            bundle: false
+        } 
+    };
+
+    var javascriptFiles = [
+        "force.js"
+    ];
+
+    var cssFiles = [
+        "forceDirected.css"
+    ]
+    
+    var paramTypes = { bundle: Boolean };
+    if (typeof Exhibit_JitExtension_urlPrefix == "string") {
+        Exhibit.JitExtension.urlPrefix = Exhibit_JitExtension_urlPrefix;
+        if ("Exhibit_JitExtension_parameters" in window) {
+            Exhibit.parseURLParameters(Exhibit_JitExtension_parameters,
+                                          Exhibit.JitExtension.params,
+                                          paramTypes);
+        }
+    } else {
+        var url = Exhibit.findScript(document, "/jit-extension.js");
+        if (url == null) {
+            Exhibit.Debug.exception(new Error("Failed to derive URL prefix for Simile Exhibit Jit Extension code files"));
+            return;
+        }
+        Exhibit.JitExtension.urlPrefix = url.substr(0, url.indexOf("jit-extension.js"));
+        
+        Exhibit.parseURLParameters(url, Exhibit.JitExtension.params, paramTypes);
+    }
+    
+    var scriptURLs = [];
+    var cssURLs = [];
+    
+    if (Exhibit.JitExtension.params.bundle) {
+        scriptURLs.push(Exhibit.JitExtension.urlPrefix + "jit-extension-bundle.js");
+        cssURLs.push(Exhibit.JitExtension.urlPrefix + "jit-extension-bundle.css");
+    } else {
+        Exhibit.prefixURLs(scriptURLs, Exhibit.JitExtension.urlPrefix + "scripts/", javascriptFiles);
+        Exhibit.prefixURLs(cssURLs, Exhibit.JitExtension.urlPrefix + "styles/", cssFiles);
+    }
+    
+    for (var i = 0; i < Exhibit.locales.length; i++) {
+        scriptURLs.push(Exhibit.JitExtension.urlPrefix + "locales/" + Exhibit.locales[i] + "/jit-locale.js");
+    };
+    
+    if (!isCompiled) {
+        Exhibit.includeJavascriptFiles(document, "", scriptURLs);
+        Exhibit.includeCssFiles(document, "", cssURLs);
+    }
+})();
